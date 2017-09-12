e8a49cbe8652a753ececd2a2547e2f2553c782d0
2017JTM2187 <bhawnakamra387@gmail.com>
2017-09-12 16:38:42 +0530

    assignment-6 problem-1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,37 @@
-###### this is the first .py file ###########
+#!/usr/bin/python
 
-####### write your code here ##########
+str2 = input(' Enter binary bit data which has to be transmitted.')
+print(str2)
+
+sub='1'
+print "str2.count(sub) : ", str2.count(sub)
+
+
+if (str2.count(sub)%2 ==0) :
+	str2 = str2 + sub
+print "binary bit data with parity bit:",str2
+
+
+pattern="010"
+
+ind = str2.index(pattern)
+
+print str2.index(pattern)
+
+ind3=ind+3
+
+seq="0"
+print str2.join(seq)
+
+#str3 = str2[0:] + str2(ind2)=0 + str2[ind+3:]
+
+print "Modified string received at the other end:",str2
+
+   
+    
+  
+
+
+
+
+
